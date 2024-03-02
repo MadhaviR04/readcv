cd67b0a16a3a31bb3e660e0a38a7c8825110d6fb
MadhaviR04 <madhaviravi04@gmail.com>
2024-03-03 03:40:51 +0530

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [jobDescription, setJobDescription] = useState('');
